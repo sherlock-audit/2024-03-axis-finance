c7b976e74c013d5ed72582ee747db80927ee3efc
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-03-18 16:18:33 +0100

    Delete scope directories

--- moonraker/script/deploy/createSafe.sh
+++
@@ -1,6 +0,0 @@
-# Load environment variables
-source .env
-
-# Deploy the safe using calculated calldata
-cast send --private-key=$DEPLOYER_PRIVATE_KEY --rpc-url=$RPC_URL --from=$DEPLOYER_ADDRESS \
-   $SAFE_FACTORY_ADDRESS $SAFE_CALLDATA
